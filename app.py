590d40bfbda0320909c02a303163311a62893b7f
Vrushabh KB <vrushabhkb@gmail.com>
2026-05-03 21:04:59 +0530

    EMAIL Iteration

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['MAIL_PORT']     = 465
 app.config['MAIL_USE_TLS']  = False
 app.config['MAIL_USE_SSL']  = True
 app.config['MAIL_USERNAME'] = 'errortranslator.att@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ztcc qylu dmno lufv'
+app.config['MAIL_PASSWORD'] = 'owpq utyv xquc fauy'
 app.config['MAIL_DEFAULT_SENDER'] = 'errortranslator.att@gmail.com'
 
 mail = Mail(app)
