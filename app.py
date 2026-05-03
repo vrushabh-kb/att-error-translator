bf4330832f33f44eae18e92218bb3909f4f79207
Vrushabh KB <vrushabhkb@gmail.com>
2026-05-03 20:39:19 +0530

    Updated EMAIL Config

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ CORS(app)
 
 # ── EMAIL CONFIG ──
 app.config['MAIL_SERVER']   = 'smtp.gmail.com'
-app.config['MAIL_PORT']     = 587
-app.config['MAIL_USE_TLS']  = True
+app.config['MAIL_PORT']     = 465
+app.config['MAIL_USE_TLS']  = False
+app.config['MAIL_USE_SSL']  = True
 app.config['MAIL_USERNAME'] = 'errortranslator.att@gmail.com'
 app.config['MAIL_PASSWORD'] = 'ztcc qylu dmno lufv'
 app.config['MAIL_DEFAULT_SENDER'] = 'errortranslator.att@gmail.com'
