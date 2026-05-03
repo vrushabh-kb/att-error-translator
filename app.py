0e2ee8e440db3c87fc2e866d51ccf6768fc4df7c
Vrushabh KB <vrushabhkb@gmail.com>
2026-05-03 11:43:10 +0530

    Add Railway DB config and requirements.txt

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ CORS(app)
 app.config['MAIL_SERVER']   = 'smtp.gmail.com'
 app.config['MAIL_PORT']     = 587
 app.config['MAIL_USE_TLS']  = True
-app.config['MAIL_USERNAME'] = 'your_gmail@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_gmail_app_password'
-app.config['MAIL_DEFAULT_SENDER'] = 'your_gmail@gmail.com'
+app.config['MAIL_USERNAME'] = 'errortranslator.att@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ztcc qylu dmno lufv'
+app.config['MAIL_DEFAULT_SENDER'] = 'errortranslator.att@gmail.com'
 
 mail = Mail(app)
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="your_mysql_password",
-        database="error_translator"
+        host     = "tramway.proxy.rlwy.net",
+        user     = "root",
+        password = "qZiqtXricNLyVSggWCbnOZsHereDKCYI",
+        database = "railway",
+        port     = 17487
     )
 
 # ── GET all error codes ──
